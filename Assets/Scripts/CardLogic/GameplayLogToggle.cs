d7968f6e868331fd84ebdd328095de7e29ad592c
Alexandraxx1-1 <alexandra.monte@stud.fh-campuswien.ac.at>
2026-06-14 02:14:16 +0200

    i button

---
+++ Assets/Scripts/CardLogic/GameplayLogToggle.cs
@@ -0,0 +1,40 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class GameplayLogToggle : MonoBehaviour
+{
+    public GameObject logTextObject;
+    public TMP_Text infoButtonText;
+
+    private bool logVisible = true;
+
+    private void Start()
+    {
+        if (logTextObject != null)
+        {
+            logVisible = logTextObject.activeSelf;
+        }
+
+        if (infoButtonText != null)
+        {
+            infoButtonText.text = "i";
+        }
+    }
+
+    public void ToggleLogText()
+    {
+        if (logTextObject == null)
+        {
+            Debug.LogWarning("LogText is not assigned.");
+            return;
+        }
+
+        logVisible = !logVisible;
+        logTextObject.SetActive(logVisible);
+
+        if (infoButtonText != null)
+        {
+            infoButtonText.text = "i";
+        }
+    }
+}
